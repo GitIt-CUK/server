a04a5ed0b84fae54b5d5a760e6e2fd3dee09f3cb
simhani1 <simhani1@gmail.com>
2023-09-03 21:26:12 +0900

    init: 프로젝트를 생성

---
+++ src/main/java/shop/gitit/GititApplication.java
@@ -0,0 +1,13 @@
+package shop.gitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GititApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GititApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop/gitit/GititApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.gitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GititApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
