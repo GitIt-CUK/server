7d523fbab139982ca83ffd8d716ec02491f8f079
Jonghan Sim <simhani1@gmail.com>
2023-09-04 02:02:13 +0900

    [Feat/#1] github-api 의존성 추가 (#2)
    * feat: github-api 의존성 추가
    
    * style: spotless 적용

--- src/main/java/shop/gitit/GititApplication.java
+++ src/main/java/shop/gitit/GititApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GititApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GititApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GititApplication.class, args);
+  }
 }
