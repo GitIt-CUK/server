7f6f8eb28ee664aded7a521b6cf4dea2ded95f56
simhani1 <simhani1@gmail.com>
2023-09-13 03:53:02 +0900

    test: 티어 단위 테스트

---
+++ src/test/java/shop/gitit/member/domain/tier/TierTest.java
@@ -0,0 +1,50 @@
+package shop.gitit.member.domain.tier;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import shop.gitit.support.fixture.member.tier.TierFixture;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static shop.gitit.member.domain.tier.Tier.*;
+
+class TierTest {
+
+    @DisplayName("커밋 수가 0이면 아이언이다")
+    @Test
+    void input0() {
+        // given
+        int commitCount = TierFixture.commitCountOf(0);
+
+        // when
+        Tier result = updateTier(commitCount);
+
+        // then
+        assertThat(result).isEqualTo(IRON);
+    }
+
+    @DisplayName("커밋 수가 103이면 골드2다")
+    @Test
+    void input103() {
+        // given
+        int commitCount = TierFixture.commitCountOf(103);
+
+        // when
+        Tier result = updateTier(commitCount);
+
+        // then
+        assertThat(result).isEqualTo(GOLD2);
+    }
+
+    @DisplayName("커밋 수가 20억이면 GOAT다")
+    @Test
+    void input200000000() {
+        // given
+        int commitCount = TierFixture.commitCountOf(2000000000);
+
+        // when
+        Tier result = updateTier(commitCount);
+
+        // then
+        assertThat(result).isEqualTo(GOAT);
+    }
+}
---
+++ src/test/java/shop/gitit/support/fixture/member/tier/TierFixture.java
@@ -0,0 +1,9 @@
+package shop.gitit.support.fixture.member.tier;
+
+public class TierFixture {
+
+
+    public static int commitCountOf(int commitCount) {
+        return commitCount;
+    }
+}
