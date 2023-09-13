055dc78e74af31633f13106cd36cb98a767e5aa4
simhani1 <simhani1@gmail.com>
2023-09-14 03:18:00 +0900

    feat: rest docs 생성 핸들러 추가

---
+++ src/test/java/shop/gitit/support/restdochandler/RestDocsHandler.java
@@ -0,0 +1,14 @@
+package shop.gitit.support.restdochandler;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+
+public class RestDocsHandler {
+
+    public static RestDocumentationResultHandler createDocument(String identifier) {
+        return document(
+                identifier, preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()));
+    }
+}
