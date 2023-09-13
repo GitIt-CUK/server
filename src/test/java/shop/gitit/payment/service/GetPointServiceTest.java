5f830835bb193dfd8a0ae835ee514fb216197e1d
simhani1 <simhani1@gmail.com>
2023-09-14 04:04:25 +0900

    test: service 단위 테스트

---
+++ src/test/java/shop/gitit/payment/service/GetPointServiceTest.java
@@ -0,0 +1,83 @@
+package shop.gitit.payment.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import shop.gitit.payment.domain.Wallet;
+import shop.gitit.payment.domain.memberId.MemberId;
+import shop.gitit.payment.exception.NoWalletException;
+import shop.gitit.payment.repository.PaymentRepository;
+import shop.gitit.payment.service.dto.GetPointDto;
+import shop.gitit.payment.service.usecase.GetPointUsecase;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {GetPointService.class})
+class GetPointServiceTest {
+
+    @Autowired private GetPointUsecase getPointUsecase;
+
+    @MockBean private PaymentRepository paymentRepository;
+
+    @DisplayName("getPoint 메서드는")
+    @Nested
+    class GetPoint {
+
+        @DisplayName("회원이 소유한 지갑이 존재한다면")
+        @Nested
+        class MemberHasWallet {
+
+            @DisplayName("지갑 소유자의 id와 잔여 포인트를 반환한다.")
+            @Test
+            void success() {
+                // given
+                GetPointDto.Request request = GetPointDto.Request.builder().memberId(1L).build();
+                MemberId ownerId = new MemberId(request.getMemberId());
+                Wallet wallet = new Wallet(ownerId);
+
+                // when
+                when(paymentRepository.findByOwnerId(any(MemberId.class)))
+                        .thenReturn(Optional.ofNullable(wallet));
+                GetPointDto.Response result = getPointUsecase.getPoint(request);
+
+                // then
+                assertThat(result)
+                        .extracting(
+                                GetPointDto.Response::getMemberId, GetPointDto.Response::getPoint)
+                        .contains(1L, 0);
+            }
+        }
+
+        @DisplayName("회원이 소유한 지갑이 존재하지 않는다면")
+        @Nested
+        class MemberHasNoWallet {
+
+            @DisplayName("예외를 던진다.")
+            @Test
+            void throwNoWalletException() {
+                // given
+                GetPointDto.Request request = GetPointDto.Request.builder().memberId(1L).build();
+                MemberId ownerId = new MemberId(request.getMemberId());
+                Wallet wallet = new Wallet(ownerId);
+
+                // when
+                when(paymentRepository.findByOwnerId(any(MemberId.class)))
+                        .thenReturn(Optional.empty());
+
+                // then
+                assertThatThrownBy(() -> getPointUsecase.getPoint(request))
+                        .isInstanceOf(NoWalletException.class);
+            }
+        }
+    }
+}
