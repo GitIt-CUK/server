e473b2acd3ca249563987361d3cd84333c88dd07
simhani1 <simhani1@gmail.com>
2023-09-14 03:38:47 +0900

    feat: 커스텀 예외 추가

---
+++ src/main/java/shop/gitit/payment/exception/NoWalletException.java
@@ -0,0 +1,19 @@
+package shop.gitit.payment.exception;
+
+public class NoWalletException extends RuntimeException {
+
+    public NoWalletException() {
+    }
+
+    public NoWalletException(String message) {
+        super(message);
+    }
+
+    public NoWalletException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NoWalletException(Throwable cause) {
+        super(cause);
+    }
+}
