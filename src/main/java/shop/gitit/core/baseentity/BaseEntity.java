6f06aab2f2084fc1c446beb976db12affa6f1439
simhani1 <simhani1@gmail.com>
2023-09-14 03:17:21 +0900

    fix:  설정 이슈로  방식으로 변경

--- src/main/java/shop/gitit/core/baseentity/BaseEntity.java
+++ src/main/java/shop/gitit/core/baseentity/BaseEntity.java
@@ -3,22 +3,40 @@ package shop.gitit.core.baseentity;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
 @MappedSuperclass
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BaseEntity {
 
-    @Column(name = "created_at")
-    @CreatedDate
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
-    @LastModifiedDate
+    @UpdateTimestamp
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
+
+    @PrePersist
+    private void prePersistFunction() {
+        LocalDateTime now = getLocalDateTime();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    private void preUpdateFunction() {
+        updatedAt = getLocalDateTime();
+    }
+
+    private LocalDateTime getLocalDateTime() {
+        return LocalDateTime.now();
+    }
 }
