6c78d4eecf0e5535f1fca5231234d489b3e64960
simhani1 <simhani1@gmail.com>
2023-09-14 03:37:16 +0900

    feat: service 구현

--- src/main/java/shop/gitit/payment/service/GetPointService.java
+++ src/main/java/shop/gitit/payment/service/GetPointService.java
@@ -1,13 +1,26 @@
 package shop.gitit.payment.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import shop.gitit.payment.domain.Wallet;
+import shop.gitit.payment.domain.memberId.MemberId;
+import shop.gitit.payment.exception.NoWalletException;
+import shop.gitit.payment.repository.PaymentRepository;
 import shop.gitit.payment.service.dto.GetPointDto;
 import shop.gitit.payment.service.usecase.GetPointUsecase;
 
 @Service
+@RequiredArgsConstructor
 public class GetPointService implements GetPointUsecase {
 
+    private final PaymentRepository paymentRepository;
+
     public GetPointDto.Response getPoint(GetPointDto.Request getPointDto) {
-        return null;
+        Wallet wallet = paymentRepository.findByOwnerId(new MemberId(getPointDto.getMemberId()))
+                .orElseThrow(() -> new NoWalletException("해당 회원의 포인트가 존재하지 않습니다."));
+        return GetPointDto.Response.builder()
+                .memberId(wallet.getOwnerId())
+                .point(wallet.getPoint())
+                .build();
     }
 }
