1d84050f200bd97206532238d6c48af2935ed2d9
simhani1 <simhani1@gmail.com>
2023-10-29 23:33:16 +0900

    feat: redis 트랜잭션 설정

--- src/main/java/shop/gitit/core/redis/config/RedisConfig.java
+++ src/main/java/shop/gitit/core/redis/config/RedisConfig.java
@@ -7,9 +7,11 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableRedisRepositories
+@EnableTransactionManagement
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
@@ -27,6 +29,7 @@ public class RedisConfig {
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
+        template.setEnableTransactionSupport(true);
         return template;
     }
 }
